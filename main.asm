1627025bdda25f0b5f2eb864897130b56568f3c0
Fabian Sörensson <fabian.sorensson@gmail.com>
2018-05-12 12:52:54 +0200

    Draw whole monkey
    I restructured the sprite-centric code, preparing for some more dynamic
    drawing. That way, I made it possible to draw the whole monkey!

--- main.asm
+++ main.asm
@@ -13,15 +13,32 @@
     .include "header.asm"
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;; Constants ;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Logic constants
+
+DIR_UP    = $00
+DIR_DOWN  = $01
+DIR_LEFT  = $02
+DIR_RIGHT = $03
+
+IDLE    = $00
+WALKING = $01
+
+; Graphics constants
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;; RAM variables ;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
   .rsset $0000
 
-monkeyX     .rs 1  ; Current X coordinate of monkey.
-monkeyImage .rs 1  ; Current sprite of monkey.
-monkeyAttr  .rs 1  ; Current monkey sprite attributes.
 monkeyY     .rs 1  ; Current Y coordinate of monkey.
+monkeyX     .rs 1  ; Current X coordinate of monkey.
+monkeyState .rs 1  ; Current monkey state (idle/walking).
+monkeyDir   .rs 1  ; Current monkey direction.
 
 controller1 .rs 1  ; Last input from controller 1.
 
@@ -57,10 +74,10 @@ LoadPalettesLoop:
     LDA #$80
     STA monkeyX
     STA monkeyY
-    LDA #$14
-    STA monkeyImage
-    LDA #$00
-    STA monkeyAttr
+    LDA #DIR_DOWN
+    STA monkeyDir
+    LDA #IDLE
+    STA monkeyState
 
 Forever:
     JMP Forever
@@ -69,22 +86,68 @@ Forever:
 ;;;;; Graphics logic ;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-NMI:
+UpdateGraphics:
     ; Start DMA transfer from $0200
     LDA #$00
     STA $2003
     LDA #$02
     STA $4014
 
-    ; Set monkey sprite in memory
-    LDA monkeyX
-    STA $0200
-    LDA monkeyImage
-    STA $0201
-    LDA monkeyAttr
-    STA $0202
-    LDA monkeyY
-    STA $0203
+    ; X will be the offset used to store all sprites at the correct
+    ; place in memory.
+    LDX #$00
+
+    JSR UpdateMonkeySprites
+
+    RTS
+
+UpdateMonkeySprites:
+    LDY #$00
+    LDA monkeyDir
+    CMP #DIR_DOWN
+    BEQ UpdateMonkeySpritesDown
+UpdateMonkeySpritesDone:
+    RTS
+
+UpdateMonkeySpritesDown:
+    LDA monkeyState
+    CMP #IDLE
+    BEQ UpdateMonkeySpritesDownIdle
+UpdateMonkeySpritesDownIdle:
+    LDA spr_monkey_down_idle, y
+    CMP #$FF ; No more sprites!
+    BEQ UpdateMonkeySpritesDone
+    CLC
+    ADC monkeyY
+    STA $0200, x
+    INX
+    INY
+
+    LDA spr_monkey_down_idle, y
+    STA $0200, x
+    INX
+    INY
+
+    LDA spr_monkey_down_idle, y
+    STA $0200, x
+    INX
+    INY
+
+    LDA spr_monkey_down_idle, y
+    CLC
+    ADC monkeyX
+    STA $0200, x
+    INX
+    INY
+
+    JMP UpdateMonkeySpritesDownIdle
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;; VBlank ;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+NMI:
+    JSR UpdateGraphics
 
     RTI
 
@@ -100,6 +163,26 @@ palette:
 
     ; Sprite palette
     .db $22,$17,$37,$0F,  $22,$00,$00,$00,  $00,$00,$00,$00,  $00,$00,$00,$00
+
+spr_monkey_down_idle:
+    ; Format: $y-offs, $tile-no, %attr, $x-offs
+    ; Upper sprite
+    .db $00, $14, %00000000, $00
+    ; Lower sprite
+    .db $08, $24, %00000000, $00
+    ; End of sprites
+    .db $FF
+
+spr_monkey_down_walking:
+    ; Number of sprites in meta-sprite
+    .db $02
+    ; Number of sprites in animation
+    .db $02
+    ; Format: $x-offs, $y-offs, [$tile-no, %attr]
+    ; Upper sprite
+    .db $00, $00,    $15, %00000000,  $15, %01000000
+    ; Lower sprite
+    .db $00, $08,    $25, %00000000,  $25, %01000000
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Interrupt vectors ;;;;
